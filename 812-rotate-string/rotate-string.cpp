34ebbdd7f7909081e34a95d48b1b17af289491c4
Lithish Kumar Gedi <149952028+Lithish779@users.noreply.github.com>
2026-05-03 23:55:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (61.66%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,8 +3,7 @@ public:
     bool rotateString(string s, string goal) {
         if(s.size()!=goal.size())return false;
         string temp=s+s;
-        return temp.find(goal)!=-1;
-
+       return (temp.find(goal)!=-1);
         
     }
 };

6793ac94f259d065aedd13c130ededf682ba5c08
Lithish Kumar Gedi <149952028+Lithish779@users.noreply.github.com>
2026-05-12 17:21:19 +0530

    Time: 1 ms (23.22%), Space: 15.5 MB (13.53%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     vector<int> luckyNumbers(vector<vector<int>>& arr) {
-        int n = arr.size();          // number of rows
-    int m = arr[0].size();
+        int n=arr.size();
+        int m=arr[0].size();
+        vector<int>rowmin(n,INT_MAX);
+        vector<int>colmax(m,INT_MIN);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                rowmin[i]=min(rowmin[i],arr[i][j]);
+                colmax[j]=max(colmax[j],arr[i][j]);
+            }
+        }
         vector<int>ans;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                bool rowmin=true;
-                bool colmax=true;
-                for(int k=0;k<m;k++){
-                    if(arr[i][k]<arr[i][j]){
-                        rowmin=false;
-                        break;
-                    }
-                }
-                for(int k=0;k<n;k++){
-                    if(arr[k][j]>arr[i][j]){
-                        colmax=false;
-                        break;
-                    }
-                }
-                if(rowmin && colmax){
+                if(arr[i][j]==rowmin[i]&&arr[i][j]==colmax[j]){
                     ans.push_back(arr[i][j]);
                 }
             }

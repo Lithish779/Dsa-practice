b724086ed359ace07d65aab4f1a719956b4e7cc3
Lithish Kumar Gedi <149952028+Lithish779@users.noreply.github.com>
2026-05-12 23:17:58 +0530

    Time: 28 ms (6.24%) | Memory: 69.7 MB (33.33%) - LeetSync

---
+++ 3206-find-common-elements-between-two-arrays/find-common-elements-between-two-arrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int>s1(nums1.begin(),nums1.end());
+        unordered_set<int>s2(nums2.begin(),nums2.end());
+        int ans=0;
+        int ans1=0;
+        for(int x:nums2){
+            if(s1.count(x)){
+                ans++;
+            }
+        }
+        for(int x: nums1){
+            if(s2.count(x)){
+                ans1++;
+            }
+        }
+        return {ans1,ans};
+
+        
+    }
+};

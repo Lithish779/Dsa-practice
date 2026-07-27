7040a5f6346111a55ca0914488220eed922e7382
Lithish Kumar Gedi <149952028+Lithish779@users.noreply.github.com>
2026-07-27 15:13:48 +0530

    Time: 5 ms (64.5%), Space: 128.2 MB (95.68%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        stack<int>pos;
-        stack<int>neg;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]>0){
-            pos.push(nums[i]);
+        int pos=0;
+        int neg=1;
+        vector<int>ans(nums.size());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>0){
+                ans[pos]=nums[i];
+                pos+=2;
+            }
+            else{
+                ans[neg]=nums[i];
+                neg+=2;
+            }
         }
-        else{
-            neg.push(nums[i]);
-        }
-    }
-    vector<int>ans;
-    while(!pos.empty()&& !neg.empty()){
-        ans.push_back(neg.top());
-        neg.pop();
-        ans.push_back(pos.top());
-        pos.pop();
-    }
-     reverse(ans.begin(),ans.end());
-    return ans;
+        return ans;
         
     }
 };
+// vector<int> rearrangeArray(vector<int>& nums) {
+//         stack<int>pos;
+//         stack<int>neg;
+//     for(int i=0;i<nums.size();i++){
+//         if(nums[i]>0){
+//             pos.push(nums[i]);
+//         }
+//         else{
+//             neg.push(nums[i]);
+//         }
+//     }
+//     vector<int>ans;
+//     while(!pos.empty()&& !neg.empty()){
+//         ans.push_back(neg.top());
+//         neg.pop();
+//         ans.push_back(pos.top());
+//         pos.pop();
+//     }
+//      reverse(ans.begin(),ans.end());
+//     return ans;
+        
+//     }

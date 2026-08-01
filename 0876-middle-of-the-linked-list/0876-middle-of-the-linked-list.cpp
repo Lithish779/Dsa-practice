6c5a3709dc5827ccd2b41929fea70984ff59fcc6
Lithish Kumar Gedi <149952028+Lithish779@users.noreply.github.com>
2026-08-01 12:12:57 +0530

    Time: 0 ms (100%), Space: 9.9 MB (64.83%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -6,16 +6,17 @@
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ * 
  */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         ListNode*slow=head;
         ListNode*fast=head;
-        while(fast!=NULL&&fast->next!=NULL){
+        while(fast!=nullptr && fast->next!=nullptr){
             slow=slow->next;
             fast=fast->next->next;
+
         }
         return slow;
         

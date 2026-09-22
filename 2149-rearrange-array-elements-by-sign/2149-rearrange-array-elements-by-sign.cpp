d0b00eddb14a0e3e16a84c718a3e3eeb4dfddb93
Lithish Kumar Gedi <149952028+Lithish779@users.noreply.github.com>
2026-09-22 23:22:27 +0530

    Time: 7 ms (55.93%), Space: 128.2 MB (78.21%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -3,8 +3,9 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int pos=0;
         int neg=1;
-        vector<int>ans(nums.size());
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size();
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
             if(nums[i]>0){
                 ans[pos]=nums[i];
                 pos+=2;
@@ -18,25 +19,3 @@ public:
         
     }
 };
-// vector<int> rearrangeArray(vector<int>& nums) {
-//         stack<int>pos;
-//         stack<int>neg;
-//     for(int i=0;i<nums.size();i++){
-//         if(nums[i]>0){
-//             pos.push(nums[i]);
-//         }
-//         else{
-//             neg.push(nums[i]);
-//         }
-//     }
-//     vector<int>ans;
-//     while(!pos.empty()&& !neg.empty()){
-//         ans.push_back(neg.top());
-//         neg.pop();
-//         ans.push_back(pos.top());
-//         pos.pop();
-//     }
-//      reverse(ans.begin(),ans.end());
-//     return ans;
-        
-//     }

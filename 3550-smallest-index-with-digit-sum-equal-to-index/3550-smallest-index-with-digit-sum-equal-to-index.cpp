d6f58846d9e55bf8e97b6461a3aabe5a1c3fb5aa
Lithish Kumar Gedi <149952028+Lithish779@users.noreply.github.com>
2026-09-25 01:08:26 +0530

    Time: 0 ms (100%), Space: 31 MB (51.82%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+int sumofdigits(int n){
+            int sum=0;
+            while(n>0){
+                sum+=n%10;
+                n=n/10;
+            }
+            return sum;
+        }
+    int smallestIndex(vector<int>& nums) {
+        
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(sumofdigits(nums[i])==i){
+                return i;
+            }
+            
+        }
+        return -1;
+        
+    }
+};
